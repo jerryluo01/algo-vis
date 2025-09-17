42af075deae26b78103fc2362d7f384db55416fb
jerryluo <jerryluogd@gmail.com>
2025-09-17 13:07:59 -0400

    added shuffle and made graph sorted in beginning

--- algo.js
+++ algo.js
@@ -1,4 +1,15 @@
-function bubble(arr) {}
+function bubble(arr) {
+    for (let i = 0; i < arr.size; i++) {
+        let swapped = false;
+        for (let j = 0; j < arr.size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
 
 function insertion(arr) {}
 
--- script.js
+++ script.js
@@ -20,29 +20,32 @@ speedSlider.addEventListener("input", () => {
     speedDisplay.textContent = speedSlider.value;
 });
 
-const newArr = document.querySelector(".new");
-newArr.addEventListener("click", () => {
-    const graphCont = document.querySelector(".graph");
-    while (graphCont.firstChild) {
-        graphCont.removeChild(graphCont.firstChild);
-    }
-    graph();
-});
+const shuffleBtn = document.querySelector(".new");
+shuffleBtn.addEventListener("click", shuffle);
 
 const sortBtn = document.querySelector(".sortBtn");
-sortBtn.addEventListener("click", sort);
+sortBtn.addEventListener("click", sortit);
 
-function generateNum() {
-    const num = Math.floor(Math.random() * 100 + 1);
-    return num;
+// function generateNum() {
+//     const num = Math.floor(Math.random() * 100 + 1);
+//     return num;
+// }
+
+function shuffle() {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
 }
 
 function generateArr() {
     const nums = document.querySelector(".nums");
     const arr = [];
-    let length = document.getElementById("length").value;
+    let length = parseInt(document.getElementById("length").value);
     for (let i = 0; i < length; i++) {
-        arr.push(generateNum());
+        arr.push(i + 1);
     }
     nums.textContent = `${arr}`;
     return arr;
@@ -51,20 +54,21 @@ function generateArr() {
 function graph() {
     arr = generateArr();
     const cont = document.querySelector(".graph");
-    let length = document.getElementById("length").value;
+    let length = parseInt(document.getElementById("length").value);
     for (let i = 0; i < length; i++) {
         const div = document.createElement("div");
         cont.appendChild(div);
         div.classList.add("bar");
-        div.style.height = `${0.6 * arr[i]}vh`;
+        div.id = `${i}`;
+        div.style.height = `${arr[i]}vh`;
     }
 }
 
-function sort(arr) {
+function sortit() {
     const method = document.getElementById("method");
     method.disabled = true;
     lenSlider.disabled = true;
-    newArr.disabled = true;
+    shuffleBtn.disabled = true;
     sortBtn.disabled = true;
     sorting = true;
 
@@ -94,7 +98,7 @@ function sort(arr) {
 
     method.disabled = false;
     lenSlider.disabled = false;
-    newArr.disabled = false;
+    shuffleBtn.disabled = false;
     sortBtn.disabled = false;
     sorting = false;
 }
